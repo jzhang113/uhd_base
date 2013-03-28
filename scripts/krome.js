20f455f73f6102bda4f926fa517f5b35f521afe9
Ken Clinkenbeard <ken-clinkenbeard@uiowa.edu>
2013-03-28 15:28:55 -0500

    Site logo fit with first menu item so as to avoid overlap.
    An anchor does not seem to take a z-index well.

---
+++ scripts/krome.js
@@ -0,0 +1,23 @@
+(function ($) {
+$(document).ready(function(){
+
+  // ----- Ken's stuff below this point -----
+
+  // Initialize main nav menu
+  // $("#block-system-main-menu ul.menu li ul.menu").hide();
+  // $("#block-system-main-menu ul.menu li.expanded ul.menu").show();
+  //$(".nav-menu a.collapse-menu").hide();
+  //$(".nav-menu h3.open").next("ul").show();
+  //$(".nav-menu h3.open").addClass("selected");  
+
+  // Open/close main nav menu buckets
+  // $(".nav-menu h3.bucket").click(function(){
+  //   if($(this).hasClass("empty") == false){  // except the empty buckets - just follow their links
+  //     $(this).next("ul").toggle();
+  //     $(this).toggleClass("selected");
+  //     return false;
+  //   }
+  // });
+
+});
+})(jQuery);

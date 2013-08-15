0bb9ea943e2615e3febfad38d150ed0105facbd7
Ken Clinkenbeard <ken-clinkenbeard@uiowa.edu>
2013-08-15 17:15:29 -0500

    uhd_base footer edits

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -301,18 +301,18 @@
                 <li class="home">
                   <a href="http://www.housing.uiowa.edu/"><img src="<?php print $GLOBALS['base_url'] . '/' . drupal_get_path('theme', 'uhd_base') . '/images/full-wordmark-wt.png'; ?>" title="University Housing &amp; Dining at The University of Iowa" alt="University Housing &amp; Dining at The University of Iowa" /></a>
                 </li>
-                <li class="admin"><h3><a href="http://housing.uiowa.edu/departments/hr/index.html">UH&amp;D Admin</a></h3>
+                <li class="admin"><h3><a href="http://housing.uiowa.edu/departments">UH&amp;D Admin</a></h3>
                   <ul>
                     <li><a href="http://housing.uiowa.edu/departments/hr/employment_opp.htm">Employment</a></li>
-                    <li><a href="http://housing.uiowa.edu/departments/index.htm">Organization</a></li>
-                    <li><a href="http://housing.uiowa.edu/departments/hr/index.html">Policies &amp; Procedures</a></li>
+                    <li><a href="http://housing.uiowa.edu/departments">Organization</a></li>
+                    <li><a href="http://housing.uiowa.edu/policy">Policies &amp; Procedures</a></li>
                   </ul>
                 </li>
                 <li class="housing"><h3><a href="http://housing.uiowa.edu">Housing</a></h3>
                   <ul>
-                    <li><a href="http://fye.uiowa.edu/admitted-now-what/living-learning-communities">Living Learning Communities</a></li>
-                    <li><a href="http://housing.uiowa.edu/departments/reslife/residencehalls.htm">Residence Halls</a></li>
-                    <li><a href="http://www.liveon.uiowa.edu/">LiveOn</a></li>
+                    <li><a href="http://housing.uiowa.edu/living-learning-communities">Living Learning Communities</a></li>
+                    <li><a href="http://housing.uiowa.edu/residence-halls">Residence Halls</a></li>
+                    <li><a href="http://housing.uiowa.edu/life-iowa">Life@Iowa</a></li>
                   </ul>
                 </li>
                 <li class="dining"><h3><a href="http://dining.uiowa.edu">Dining</a></h3>
